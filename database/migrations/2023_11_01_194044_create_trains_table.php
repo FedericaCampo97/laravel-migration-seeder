ebfb0bcbc2ab7022ebf0d777ea83acc9fac8b2ca
FedericaCampo97 <federica.campo97@gmail.com>
2023-11-01 20:59:28 +0100

    Create migrations

---
+++ database/migrations/2023_11_01_194044_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string("agency");
+            $table->string("starts");
+            $table->string("arrivals");
+            $table->dateTime('hour_starts');
+            $table->dateTime('hour_arrivals');
+            $table->integer('code_train');
+            $table->integer('number_ carriages');
+            $table->boolean("in_time");
+            $table->boolean("deleted");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
